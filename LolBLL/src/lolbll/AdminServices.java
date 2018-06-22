175206fcbdb6fd7ef595c55c234eb7b7727beb8e
João Ferreira <joaocferreira10@hotmail.com>
2018-06-22 16:47:05 +0100

    Added AdminServices

---
+++ LolBLL/src/lolbll/AdminServices.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lolbll;
+
+import java.util.List;
+import loldal.model.Admin;
+
+/**
+ *
+ * @author joaoc
+ */
+public class AdminServices {
+
+    public AdminServices() {
+    }
+    
+    public static List<Admin> listaAdmins(){
+        List<Admin> admins = hibernate.HibernateGenericLib.executeHQLQuery("from Admin");
+        
+        return admins;
+    }
+    
+}
