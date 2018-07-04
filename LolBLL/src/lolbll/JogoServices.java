a572d481a5a456e1bb0a6b33f76b909b64a80949
João Ferreira <joaocferreira10@hotmail.com>
2018-07-05 00:10:08 +0100

    update Jogos Services

--- LolBLL/src/lolbll/JogoServices.java
+++ LolBLL/src/lolbll/JogoServices.java
@@ -6,6 +6,7 @@
 package lolbll;
 
 import hibernate.HibernateGenericLib;
+import java.math.BigDecimal;
 import java.util.List;
 import loldal.model.Equipa;
 import loldal.model.Jogo;
@@ -37,5 +38,31 @@ public class JogoServices {
     
     public static void updateJogo(Jogo j){
         HibernateGenericLib.updateObject(j);
+    }
+    
+    public static void addDeathsEquipa1(Jogo j, int deaths){
+        j.setDeathsequipa1(new BigDecimal(j.getDeathsequipa1().intValue() + deaths));
+        updateJogo(j);
+    }
+    public static void addDeathsEquipa2(Jogo j, int deaths){
+        j.setDeathequipa2(new BigDecimal(j.getDeathequipa2().intValue() + deaths));
+        updateJogo(j);
+    }
+    
+    public static void addKillsEquipa1(Jogo j, int kills){
+        j.setKillsequipa1(new BigDecimal(j.getKillsequipa1().intValue() + kills));
+        updateJogo(j);
+    }
+    public static void addKillsEquipa2(Jogo j, int kills){
+        j.setKillsequipa2(new BigDecimal(j.getKillsequipa2().intValue() + kills));
+        updateJogo(j);
+    }
+    public static void addAssistsEquipa1(Jogo j, int assists){
+        j.setAssistsequipa1(new BigDecimal(j.getAssistsequipa1().intValue() + assists));
+        updateJogo(j);
+    }
+    public static void addAssistsEquipa2(Jogo j, int assists){
+        j.setAssistsequipa2(new BigDecimal(j.getAssistsequipa2().intValue() + assists));
+        updateJogo(j);
     }
 }
